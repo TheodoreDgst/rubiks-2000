20dea8d859b182b2241bcef3e2d655f45c3f77c7
Théodore DEGUEST <theodore.deguest@epita.fr>
2024-02-07 20:22:49 +0100

    add cube movment

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 mod cube;
+use std::vec;
+
 use cube::cube::Cube;
 
 use crate::cube::cube::Color;
 
 
 fn main() {
-    let mut c = Cube::new(3);
+    let mut c = Cube::new_filled(3);
     c.dbg();
-    c.faces[0][1] = Color::Green;
-    c.faces[0][3] = Color::Blue;
-    c.dbg();
-    let v = c.get_row(0, 0);
-    dbg!(v);
+    c.shuffle(100);
+    c.dbg()
 }
